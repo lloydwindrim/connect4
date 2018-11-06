e868f35ce6ca56b3b27d6bd47229b3c3124ceabd
lloydwindrim <44772188+lloydwindrim@users.noreply.github.com>
2018-11-06 11:37:14 +1100

    Add files via upload

---
+++ template_bot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import useful_tools
+
+def bot( gameState , round, playerID ):
+    # ---------------------------------------------------
+    # description:
+    #   randomly chooses a column to place token
+    # inputs:
+    #   gameState ==> a copy of the current board (incl. tokens)
+    #   round ==> the iteration number for the game (starting at 1)
+    #   playerID ==> whether player is 1 or 2
+    # outputs:
+    #   col ==> column location to place token (between 1 and boardSize)
+    # ---------------------------------------------------
+
+ 	
+	
+
+    return col
+
+
+
+
